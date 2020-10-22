f364c33887a82f946c3698c42d762f996596e29a
Subramani A C <subramani.ac@promantia.com>
2020-10-22 13:39:09 +0530

    [7564/7566/7568/7570/7572/7573/7602/7605/7610]added new doctype employee resignation and workflows for leave application, employee advance, payroll entry,employee separation, employee resignation and notifications for the same along with SO and SI and custom fields

---
+++ axis_inspection/axis_inspection/doctype/employee_resignation/employee_resignation.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2020, veena and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Employee Resignation', {
+	department:function(frm,cdt,cdn){
+    		frappe.db.get_value("Department",frm.doc.department,"department_manager",(c)=>{
+	    		if(c.department_manager!==null){
+	        		var department_manager=c.department_manager;
+    	   			frappe.db.get_value("Employee",department_manager,"user_id",(r)=>{
+        	    		if(r.user_id===null){
+        	        		frm.doc.department_manager='';
+        	    		}else{
+        	        		frm.doc.department_manager=r.user_id;
+        	    		}
+        	    		cur_frm.refresh_fields("department_manager")
+    	   			});
+	    		}
+    		});
+	}
+});
