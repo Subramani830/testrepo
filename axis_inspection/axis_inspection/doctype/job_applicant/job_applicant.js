9dd41bf8996eb57be8ff9b7a3a279b79969d5c66
sagarikanaik <sagarika.naik@promantia.com>
2021-01-21 18:54:37 +0530

    8040/Modified job_applicant.js file, removed unnecessary code

--- axis_inspection/axis_inspection/doctype/job_applicant/job_applicant.js
+++ axis_inspection/axis_inspection/doctype/job_applicant/job_applicant.js
@@ -1,28 +1,4 @@
 frappe.ui.form.on('Job Applicant', {
-refresh:function(frm,cdt,cdn){
-    var sender=get_sender();
-    if(frm.doc.status=="Accepted"){
-        var emailTemplate=
-        '<h3>Dear ' +frm.doc.applicant_name+ ',</h3>'+
-        '<h3>    Your Job application for '+frm.doc.job_title+' has been accepted. Please note the interview date and details as scheduled below.</h3>'+
-        '<h3>1. Interview date:'+moment(frm.doc.interview_date).format('DD-MM-YYYY')+'</h3>'+
-        '<h3>2. Interview time:'+frm.doc.interview_time+'</h3>'+
-        '<h3>Please confirm receipt and your attendance.</h3>'+
-        '<br>'+
-        '<h3>Regards</h3>'+
-        '<h3>HR Team</h3>';
-        sendEmail(sender,frm.doc.name,frm.doc.email_id,emailTemplate)
-    }
-    else if(frm.doc.status=="Rejected"){
-        var emailTemplate=
-        '<h3>Dear ' +frm.doc.applicant_name+ ',</h3>'+
-        '<h3>This is regarding your job application '+frm.doc.job_title+' . We are sorry to inform you that at this time, we will not be able consider your application for the post. we will revert back when a suitable position is available. we are keeping the record of your data in our database.</h3>'+
-        '<br>'+
-        '<h3>Thanks and Regards</h3>'+
-        '<h3>HR Team</h3>';
-        sendEmail(sender,frm.doc.name,frm.doc.email_id,emailTemplate)
-    }
-},
 interview_date:function(frm,cdt,cd){
     var today = new Date().toISOString().slice(0, 10)
         if(frm.doc.interview_date<today){
@@ -48,40 +24,3 @@ department:function(frm,cdt,cdn){
 }
 });
 
-
-function sendEmail(sender,name,email,template){
-    frappe.call({
-            method: "frappe.core.doctype.communication.email.make",
-            args: {
-                sender:sender,
-                subject: name,
-                communication_medium: "Email",
-                recipients: email,
-                content: template,
-                communication_type: "Communication",
-                send_email:1
-                },
-            callback: function(rh){
-                console.log("sent");
-             }   
-    });
-}
-
-function  get_sender(){
-    var sender;
-    frappe.call({
-        method:"axis_inspection.axis_inspection.api.get_sender_email",
-        async:false,
-        args: {
-            doctype: "Has Role",
-            role:"HR Manager",
-            parenttype:"User"
-        },
-            callback: function(r) {
-                if(r.message!==undefined){
-                    sender=r.message;
-                }
-            }
-    });
-    return sender
-}
