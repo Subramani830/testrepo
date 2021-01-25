35f056980f47893ad28fefb8b22bc51066742b63
veena56 <veena.h@promantia.com>
2021-01-25 19:04:16 +0530

    8040/bugfix

--- axis_inspection/axis_inspection/doctype/delivery_note/delivery_note.js
+++ axis_inspection/axis_inspection/doctype/delivery_note/delivery_note.js
@@ -1,10 +1,10 @@
 frappe.ui.form.on('Delivery Note', {
 before_save:  function(frm) {
     $.each(frm.doc.items,function(idx,item){
-        frappe.db.get_value("Sales Order",{"name":item.sales_order},"delivery_date",(r)=>{
+        frappe.db.get_value("Sales Order",{"name":item.against_sales_order},"delivery_date",(r)=>{
             if(r.delivery_date<=frappe.datetime.nowdate()){
                 frappe.validated=false;
-                frappe.msgprint(__("Sales Order "+item.sales_order+" has been expired."));    
+                frappe.msgprint(__("Sales Order "+item.against_sales_order+" has been expired."));    
             }
         });
     }); 
