b47039755192b94d841bf557950af591e556e270
veena56 <veena.h@promantia.com>
2021-04-24 17:16:12 +0530

    bugfix

--- axis_inspection/axis_inspection/doctype/employee_costs/employee_costs.py
+++ axis_inspection/axis_inspection/doctype/employee_costs/employee_costs.py
@@ -11,10 +11,4 @@ from frappe.model.mapper import get_mapped_doc
 class IncompleteTaskError(frappe.ValidationError): pass
 
 class EmployeeCosts(Document):
-	def validate(self):
-		self.validate_duplicate_employee_costs()
-
-	def validate_duplicate_employee_costs(self):
-		emp_costs = frappe.db.exists("Employee Costs",{"employee": self.employee})
-		if emp_costs and emp_costs != self.name:
-			frappe.throw(_("Employee Costs: {0} is already for Employee: {1}").format(frappe.bold(emp_costs), frappe.bold(self.employee)))
+	pass
