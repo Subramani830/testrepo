d10c14111ce83aeb53208f112c0d118dd084f461
veena56 <veena.h@promantia.com>
2021-06-03 11:23:25 +0530

    8448/changes in salary_slip and attendance and also bugfix

--- axis_inspection/axis_inspection/doctype/journal_entry/journal_entry.js
+++ axis_inspection/axis_inspection/doctype/journal_entry/journal_entry.js
@@ -1,18 +1,47 @@
 frappe.ui.form.on('Journal Entry', {
-	refresh(frm) {
-		// your code here
+	get_employee_costs: function (frm) {
+		if (frm.doc.employee != undefined) {
+			frappe.call({
+				method: "axis_inspection.axis_inspection.doctype.journal_entry.journal_entry.add_accounting_entries",
+				async: false,
+				args: {
+					employee: frm.doc.employee,
+					posting_date: frm.doc.posting_date
+				},
+				callback: function (r) {
+					frm.clear_table("accounts");
+				$.each(r.message, function (i, d) {
+						var c = frm.add_child("accounts");
+						//c.party_type = "Employee",
+							//c.party = frm.doc.employee,
+							c.reference_type = "Employee Costs",
+							c.reference_name = d.name,
+							c.debit_in_account_currency = d.total_cost
+					});
+					$.each(r.message, function (i, d) {
+						var c = frm.add_child("accounts");
+						//c.party_type = "Employee",
+							//c.party = frm.doc.employee,
+							c.reference_type = "Employee Costs",
+							c.reference_name = d.name,
+							c.credit_in_account_currency = d.total_cost
+					});
+					cur_frm.refresh_fields("accounts");
+				}
+			});
+		}
 	}
 })
 
 frappe.ui.form.on('Journal Entry Account', {
-	accounts_add(frm,cdt,cdn) {
-	    var cur_grid =frm.get_field('accounts').grid;
+	accounts_add(frm, cdt, cdn) {
+		var ctur_grid = frm.get_field('accounts').grid;
 		var cur_doc = locals[cdt][cdn];
 		var cur_row = cur_grid.get_row(cur_doc.name);
-		if (frm.doc.reference_no!=undefined){
-    		cur_row.doc.reference_type="Employee Costs"
-    		cur_row.doc.reference_name=frm.doc.reference_no
-    		cur_frm.refresh_fields();
+		if (frm.doc.reference_no != undefined) {
+			cur_row.doc.reference_type = "Employee Costs"
+			cur_row.doc.reference_name = frm.doc.reference_no
+			cur_frm.refresh_fields();
 		}
 	}
 })
--- axis_inspection/axis_inspection/doctype/payment_entry/payment_entry.js
+++ axis_inspection/axis_inspection/doctype/payment_entry/payment_entry.js
@@ -1,198 +1,247 @@
 frappe.ui.form.on('Payment Entry', {
-    refresh:function(frm){
-        $.each(frm.doc.references,function(idx, item){
-            if(item.reference_doctype=="Purchase Invoice"){
-                frappe.call({
-                    method:"axis_inspection.axis_inspection.api.update_project",
-                    async:false,
-                    args:{
-                        doctype:'Purchase Invoice Item',
-                        name:item.reference_name,
-                        parenttype:'Purchase Invoice'
-                    },
-                    callback:function(r){
-			var project;
-			var task;
-			var branch;
-			var cost_center;
-			for(var i=0;i<r.message.length;i++){
-				if(r.message[i].project){project=r.message[i].project}
-				if(r.message[i].task){task=r.message[i].task}
-				if(r.message[i].cost_center){cost_center=r.message[i].cost_center}
-				if(r.message[i].branch){branch=r.message[i].branch}
-			
-			
-                        //frappe.model.set_value(item.doctype, item.name, "project_name", r.message[i].project);
-                        //frappe.model.set_value(item.doctype, item.name, "task", r.message[i].task);
-			//frappe.model.set_value(item.doctype, item.name, "cost_center"r.message[i].task);
-                        //frappe.model.set_value(item.doctype, item.name, "branch", r.message[i].branch);
-                    }
-                        if(frm.doc.cost_center){}
-			else{
-			frm.set_value("cost_center",cost_center)}
-			if(frm.doc.project){}
-			else{frm.set_value("project",project)}
-			if(frm.doc.branch){}
-			else{frm.set_value("branch",branch)}
-			if(frm.doc.task){}
-			else{frm.set_value("task",task)}
-                    }
-                });
-		if(frm.doc.division==undefined){
-			frappe.db.get_value("Purchase Invoice", {"name":item.reference_name},"division",(d)=>{
-				frm.set_value("division",d.division)
-		
-		})
-		}
-            }
-	else if(item.reference_doctype=="Purchase Order"){
-                frappe.call({
-                    method:"axis_inspection.axis_inspection.api.update_project",
-                    async:false,
-                    args:{
-                        doctype:'Purchase Order Item',
-                        name:item.reference_name,
-                        parenttype:'Purchase Order'
-                    },
-                    callback:function(r){
-			var project;
-			var task;
-			var branch;
-			var cost_center;
-			for(var i=0;i<r.message.length;i++){
-				if(r.message[i].project){project=r.message[i].project}
-				if(r.message[i].task){task=r.message[i].task}
-				if(r.message[i].cost_center){cost_center=r.message[i].cost_center}
-				if(r.message[i].branch){branch=r.message[i].branch}
+	refresh: function (frm) {
+		$.each(frm.doc.references, function (idx, item) {
+			if (item.reference_doctype == "Purchase Invoice") {
+				frappe.call({
+					method: "axis_inspection.axis_inspection.api.update_project",
+					async: false,
+					args: {
+						doctype: 'Purchase Invoice Item',
+						name: item.reference_name,
+						parenttype: 'Purchase Invoice'
+					},
+					callback: function (r) {
+						var project;
+						var task;
+						var branch;
+						var cost_center;
+						for (var i = 0; i < r.message.length; i++) {
+							if (r.message[i].project) { project = r.message[i].project }
+							if (r.message[i].task) { task = r.message[i].task }
+							if (r.message[i].cost_center) { cost_center = r.message[i].cost_center }
+							if (r.message[i].branch) { branch = r.message[i].branch }
 
-			
-                        //frappe.model.set_value(item.doctype, item.name, "project_name", r.message[i].project);
-                        //frappe.model.set_value(item.doctype, item.name, "task", r.message[i].task);
-			//frappe.model.set_value(item.doctype, item.name, "cost_center"r.message[i].task);
-                        //frappe.model.set_value(item.doctype, item.name, "branch", r.message[i].branch);
-                    }
-                        if(frm.doc.cost_center){}
-			else{
-			frm.set_value("cost_center",cost_center)}
-			if(frm.doc.project){}
-			else{frm.set_value("project",project)}
-			if(frm.doc.branch){}
-			else{frm.set_value("branch",branch)}
-			if(frm.doc.task){}
-			else{frm.set_value("task",task)}
-                    }
-                });
-            }
-	else if(item.reference_doctype=="Sales Invoice"){
-                frappe.call({
-                    method:"axis_inspection.axis_inspection.api.update_project1",
-                    async:false,
-                    args:{
-                        doctype:'Sales Invoice',
-                        name:item.reference_name
-                    },
-                    callback:function(r){
-			var project;
-			var task;
-			var branch;
-			var cost_center;
-			var contract
-			for(var i=0;i<r.message.length;i++){
-				if(r.message[i].project){project=r.message[i].project}
-				if(r.message[i].cost_center){cost_center=r.message[i].cost_center}
-				if(r.message[i].branch){branch=r.message[i].branch}
-				if(r.message[i].contract){contract=r.message[i].contract}
-                    }
-                        if(frm.doc.cost_center){}
-			else{
-			frm.set_value("cost_center",cost_center)}
-			if(frm.doc.project){}
-			else{frm.set_value("project",project)}
-			if(frm.doc.branch){}
-			else{frm.set_value("branch",branch)}
-			if(frm.doc.contract){}
-			else{frm.set_value("contract",contract)}
-                    }
-                });
-            }
-else if(item.reference_doctype=="Sales Order"){
-                frappe.call({
-                    method:"axis_inspection.axis_inspection.api.update_project2",
-                    async:false,
-                    args:{
-                        doctype:'Sales Order Item',
-                        name:item.reference_name,
-                        parenttype:'Sales Order'
-                    },
-                    callback:function(r){
-			var project;
-			var task;
-			var branch;
-			var cost_center;
-			for(var i=0;i<r.message.length;i++){
-				if(r.message[i].project){project=r.message[i].project}
-				if(r.message[i].task){task=r.message[i].task}
-				if(r.message[i].cost_center){cost_center=r.message[i].cost_center}
-				if(r.message[i].branch){branch=r.message[i].branch}
-                    }
-                        if(frm.doc.cost_center){}
-			else{
-			frm.set_value("cost_center",cost_center)}
-			if(frm.doc.project){}
-			else{frm.set_value("project",project)}
-			if(frm.doc.branch){}
-			else{frm.set_value("branch",branch)}
-                    }
-                });
-            }
-        });
-    },
-before_save:  function(frm) {
-    $.each(frm.doc.references,function(idx,item){
-	if(item.reference_name!=undefined){
-		frappe.db.get_value("Sales Order",{"name":item.reference_name},"delivery_date",(r)=>{
-		    if(r.delivery_date<=frappe.datetime.nowdate()){
-		        frappe.validated=false;
-		        frappe.msgprint(__("Sales Order "+item.reference_name+" has been expired."));    
-		    }
+
+							//frappe.model.set_value(item.doctype, item.name, "project_name", r.message[i].project);
+							//frappe.model.set_value(item.doctype, item.name, "task", r.message[i].task);
+							//frappe.model.set_value(item.doctype, item.name, "cost_center"r.message[i].task);
+							//frappe.model.set_value(item.doctype, item.name, "branch", r.message[i].branch);
+						}
+						if (frm.doc.cost_center) { }
+						else {
+							frm.set_value("cost_center", cost_center)
+						}
+						if (frm.doc.project) { }
+						else { frm.set_value("project", project) }
+						if (frm.doc.branch) { }
+						else { frm.set_value("branch", branch) }
+						if (frm.doc.task) { }
+						else { frm.set_value("task", task) }
+					}
+				});
+				if (frm.doc.division == undefined) {
+					frappe.db.get_value("Purchase Invoice", { "name": item.reference_name }, "division", (d) => {
+						frm.set_value("division", d.division)
+
+					})
+				}
+			}
+			else if (item.reference_doctype == "Purchase Order") {
+				frappe.call({
+					method: "axis_inspection.axis_inspection.api.update_project",
+					async: false,
+					args: {
+						doctype: 'Purchase Order Item',
+						name: item.reference_name,
+						parenttype: 'Purchase Order'
+					},
+					callback: function (r) {
+						var project;
+						var task;
+						var branch;
+						var cost_center;
+						for (var i = 0; i < r.message.length; i++) {
+							if (r.message[i].project) { project = r.message[i].project }
+							if (r.message[i].task) { task = r.message[i].task }
+							if (r.message[i].cost_center) { cost_center = r.message[i].cost_center }
+							if (r.message[i].branch) { branch = r.message[i].branch }
+
+
+							//frappe.model.set_value(item.doctype, item.name, "project_name", r.message[i].project);
+							//frappe.model.set_value(item.doctype, item.name, "task", r.message[i].task);
+							//frappe.model.set_value(item.doctype, item.name, "cost_center"r.message[i].task);
+							//frappe.model.set_value(item.doctype, item.name, "branch", r.message[i].branch);
+						}
+						if (frm.doc.cost_center) { }
+						else {
+							frm.set_value("cost_center", cost_center)
+						}
+						if (frm.doc.project) { }
+						else { frm.set_value("project", project) }
+						if (frm.doc.branch) { }
+						else { frm.set_value("branch", branch) }
+						if (frm.doc.task) { }
+						else { frm.set_value("task", task) }
+					}
+				});
+			}
+			else if (item.reference_doctype == "Sales Invoice") {
+				frappe.call({
+					method: "axis_inspection.axis_inspection.api.update_project1",
+					async: false,
+					args: {
+						doctype: 'Sales Invoice',
+						name: item.reference_name
+					},
+					callback: function (r) {
+						var project;
+						var task;
+						var branch;
+						var cost_center;
+						var contract
+						for (var i = 0; i < r.message.length; i++) {
+							if (r.message[i].project) { project = r.message[i].project }
+							if (r.message[i].cost_center) { cost_center = r.message[i].cost_center }
+							if (r.message[i].branch) { branch = r.message[i].branch }
+							if (r.message[i].contract) { contract = r.message[i].contract }
+						}
+						if (frm.doc.cost_center) { }
+						else {
+							frm.set_value("cost_center", cost_center)
+						}
+						if (frm.doc.project) { }
+						else { frm.set_value("project", project) }
+						if (frm.doc.branch) { }
+						else { frm.set_value("branch", branch) }
+						if (frm.doc.contract) { }
+						else { frm.set_value("contract", contract) }
+					}
+				});
+			}
+			else if (item.reference_doctype == "Sales Order") {
+				frappe.call({
+					method: "axis_inspection.axis_inspection.api.update_project2",
+					async: false,
+					args: {
+						doctype: 'Sales Order Item',
+						name: item.reference_name,
+						parenttype: 'Sales Order'
+					},
+					callback: function (r) {
+						var project;
+						var task;
+						var branch;
+						var cost_center;
+						for (var i = 0; i < r.message.length; i++) {
+							if (r.message[i].project) { project = r.message[i].project }
+							if (r.message[i].task) { task = r.message[i].task }
+							if (r.message[i].cost_center) { cost_center = r.message[i].cost_center }
+							if (r.message[i].branch) { branch = r.message[i].branch }
+						}
+						if (frm.doc.cost_center) { }
+						else {
+							frm.set_value("cost_center", cost_center)
+						}
+						if (frm.doc.project) { }
+						else { frm.set_value("project", project) }
+						if (frm.doc.branch) { }
+						else { frm.set_value("branch", branch) }
+					}
+				});
+			}
+			var bank_account = get_bank_account(item.reference_doctype, item.reference_name)
+			//frappe.db.get_value(item.reference_doctype, { 'name': item.reference_name }, "bank_account", (r) => {
+			if (item.reference_doctype == "Purchase Invoice") {
+				frm.set_value("bank_account", bank_account)
+			}
+			else if (item.reference_doctype == "Sales Invoice") {
+				frm.set_value("bank_account", bank_account)
+			}
+			//});
 		});
+
+	},
+	before_save: function (frm) {
+		var bank_account;
+		$.each(frm.doc.references, function (idx, item) {
+			if (item.reference_name != undefined) {
+				frappe.db.get_value("Sales Order", { "name": item.reference_name }, "delivery_date", (r) => {
+					if (r.delivery_date <= frappe.datetime.nowdate()) {
+						frappe.validated = false;
+						frappe.msgprint(__("Sales Order " + item.reference_name + " has been expired."));
+					}
+				});
+				if (item.reference_doctype == "Sales Invoice" || item.reference_doctype == "Purchase Invoice") {
+					if (bank_account == undefined) {
+						bank_account = get_bank_account(item.reference_doctype, item.reference_name)
+						frm.set_value("bank_account", bank_account)
+					}
+					else {
+						var account = get_bank_account(item.reference_doctype, item.reference_name)
+						if (bank_account != account) {
+							frappe.throw(item.reference_doctype + ' ' + item.reference_name + ' should be allow for bank_Account  "' + account + '".');
+						}
+					}
+				}
+			}
+
+		});
+	},
+	employee_costs: function (frm) {
+		if (frm.doc.employee_costs != undefined && frm.doc.party == undefined) {
+			frappe.db.get_value("Employee Costs", { "name": frm.doc.employee_costs }, "employee", (e) => {
+				frm.set_value("party_type", "Employee")
+				frm.set_value("party", e.employee)
+				cur_frm.refresh_field("party");
+				frm.set_value("payment_type", "Pay")
+			});
+		}
+	},
+	on_submit: function (frm) {
+		if (frm.doc.employee_costs != undefined && frm.doc.employee_costs != null) {
+			frappe.call({
+				"method": "frappe.client.set_value",
+				"args": {
+					"doctype": "Employee Costs",
+					"name": frm.doc.employee_costs,
+					"fieldname": {
+						"status": "Paid"
+					},
+				}
+			})
+		}
 	}
-    }); 
-},
-employee_costs: function(frm){
-	if(frm.doc.employee_costs!=undefined && frm.doc.party==undefined){
-	frappe.db.get_value("Employee Costs",{"name":frm.doc.employee_costs},"employee",(e)=>{
-	frm.set_value("party_type","Employee")
-	frm.set_value("party",e.employee)
-	cur_frm.refresh_field("party");
-	frm.set_value("payment_type","Pay")
-	});
-	}
-},
-on_submit: function(frm){
-	if(frm.doc.employee_costs!=undefined && frm.doc.employee_costs != null){
-		    frappe.call({
-                "method": "frappe.client.set_value",
-                "args": {
-                    "doctype": "Employee Costs",
-                    "name": frm.doc.employee_costs,
-                "fieldname": {
-                    "status":"Paid"
-                },
-                }
-            })
-}
-}
 });
 
 frappe.ui.form.on('Payment Entry Reference', {
-	references_add(frm,cdt,cdn) {
-	    var cur_grid =frm.get_field('references').grid;
+	references_add(frm, cdt, cdn) {
+		var cur_grid = frm.get_field('references').grid;
 		var cur_doc = locals[cdt][cdn];
 		var cur_row = cur_grid.get_row(cur_doc.name);
-		if (frm.doc.employee_costs!=undefined){
-    		cur_row.doc.employee_costs=frm.doc.employee_costs
-    		cur_frm.refresh_fields();
+		if (frm.doc.employee_costs != undefined) {
+			cur_row.doc.employee_costs = frm.doc.employee_costs
+			cur_frm.refresh_fields();
 		}
 	}
 })
+
+function get_bank_account(doctype, name) {
+	var account;
+	frappe.call({
+		method: 'frappe.client.get_value',
+		"async": false,
+		args: {
+			'doctype': doctype,
+			'filters': {
+				'name': name
+			},
+			'fieldname': [
+				"bank_account"
+			]
+		},
+		callback: function (r) {
+			account = r.message.bank_account
+		}
+	});
+	return account
+}
