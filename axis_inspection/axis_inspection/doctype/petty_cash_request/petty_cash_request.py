10a97a542e1cf5ed60deb3fe3f30feff07b93dd4
IshwaryaM3 <ishwarya.m@promantia.com>
2021-10-01 17:09:33 +0530

    [8739/Petty Cash Request]Adding the bugfixes

--- axis_inspection/axis_inspection/doctype/petty_cash_request/petty_cash_request.py
+++ axis_inspection/axis_inspection/doctype/petty_cash_request/petty_cash_request.py
@@ -11,7 +11,13 @@ def create_payment_entry_record(doc):
     payment_entry_record.payment_type='Internal Transfer'
     payment_entry_record.paid_to=pcr_record['petty_cash_account']
     payment_entry_record.petty_cash_request=pcr_record['name']
-    payment_entry_record.insert(ignore_mandatory=True)
+    account_rec= frappe.get_doc('Account',pcr_record['petty_cash_account'])
+    payment_entry_record.paid_to_account_currency=account_rec.account_currency
+    payment_entry_record.flags.ignore_mandatory = True
+    payment_entry_record.flags.ignore_validate = True
+    payment_entry_record.flags.ignore_permissions = True
+    payment_entry_record.save()
+    #payment_entry_record.insert(ignore_mandatory=True)
     return payment_entry_record.name
 
 def update_petty_cash_request_status(doc,method=None):
